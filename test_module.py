427a158abefda8c3522fdd8a8fdd5f0f13b226bd
Farid <feridquliyev8@gmail.com>
2023-02-14 13:06:57 +0400

    test program was change a little bit

--- test_module.py
+++ test_module.py
@@ -19,11 +19,13 @@ class TestHeuristic(unittest.TestCase):
         
         #Test if the estimated cost from test_state_1 to target_quantity is less than or equal 
         #to the total cost from test_state_1 to test_state_2 and from test_state_2 to target_quantity
-        self.assertLessEqual(heuristic(test_state_1, target_quantity), heuristic(test_state_1, target_quantity) + heuristic(test_state_2, target_quantity))
+        self.assertLessEqual(heuristic(test_state_1, target_quantity), 
+                             heuristic(test_state_1, target_quantity) + heuristic(test_state_2, target_quantity))
 
         #Test if the estimated cost from test_state_2 to target_quantity is less than or equal 
         #to the total cost from test_state_2 to test_state_3 and from test_state_3 to target_quantity
-        self.assertLessEqual(heuristic(test_state_2, target_quantity), heuristic(test_state_2, target_quantity) + heuristic(test_state_3, target_quantity))
+        self.assertLessEqual(heuristic(test_state_2, target_quantity), 
+                             heuristic(test_state_2, target_quantity) + heuristic(test_state_3, target_quantity))
 
 
 #Testing the A star algorithm with input examples of capacities and target quantity
