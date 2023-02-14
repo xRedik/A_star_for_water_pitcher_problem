64b1e8e959ae1684f02f0e41f9fff80c702c7256
Farid Guliyev <56725845+xRedik@users.noreply.github.com>
2023-02-14 14:50:09 +0400

    Add files via upload

--- test_module.py
+++ test_module.py
@@ -1,24 +1,32 @@
 import unittest
 from water_pitcher import heuristic, a_star_algorithm
 
+#Testing the heuristic function
 class TestHeuristic(unittest.TestCase):
+
+    #Test if the heuristic never overestimates the actual cost
     def test_admissible(self):
-        # Test if the heuristic never overestimates the actual cost
         for i in range(1, 10):
             self.assertLessEqual(heuristic((i, 0), 10), 10 - i)
     
+    
+    #Test if the heuristic is consistent
     def test_consistent(self):
         test_state_1 = [2, 0, 6, 72, 10]
         test_state_2 = [2, 5, 6, 72, 10]
         test_state_3 = [0, 5, 6, 72, 12]
         target_quantity = 143
         
-        # Test if the estimated cost from state1 to target_quantity is less than or equal to the sum of the cost from state1 to state2 and from state2 to target_quantity
+        #Test if the estimated cost from test_state_1 to target_quantity is less than or equal 
+        #to the total cost from test_state_1 to test_state_2 and from test_state_2 to target_quantity
         self.assertLessEqual(heuristic(test_state_1, target_quantity), heuristic(test_state_1, target_quantity) + heuristic(test_state_2, target_quantity))
 
-        # Test if the estimated cost from state2 to target_quantity is less than or equal to the sum of the cost from state2 to state3 and from state3 to target_quantity
+        #Test if the estimated cost from test_state_2 to target_quantity is less than or equal 
+        #to the total cost from test_state_2 to test_state_3 and from test_state_3 to target_quantity
         self.assertLessEqual(heuristic(test_state_2, target_quantity), heuristic(test_state_2, target_quantity) + heuristic(test_state_3, target_quantity))
 
+
+#Testing the A star algorithm with input examples of capacities and target quantity
 class TestAStar(unittest.TestCase):
     def test_input_1(self):
         capacities = [2]
--- water_pitcher.py
+++ water_pitcher.py
@@ -3,70 +3,105 @@ import numpy as np
 import heapq
 import os
 
+#function for reading the file and collecting the capacities to numpy array
+#and converting the target quantity to integer value
 def read_file(filename):
     with open(filename,"r") as f:
         capacities = np.array(f.readline().split(","))
         target = int(f.readline())
     return capacities.astype(np.int32), target
 
+#function for calculating the herustic by subtracting the total states with the target quantity
 def heuristic(current_state,target_quantity):
     return np.abs(np.sum(current_state) - target_quantity)
 
-def a_star_algorithm(capacities,target_quantity):
-    capacities = np.append(capacities, sys.maxsize)
-    initial_state = list(np.zeros(capacities.shape, dtype = np.int32))
-    initial_heuristic = heuristic(initial_state,target_quantity)
-    step = 0
 
-    fridge_pq = [(initial_heuristic+step, step, initial_state)]
+#function for solving the shortest path using A star algorithm
+def a_star_algorithm(capacities,target_quantity):
+    #appending the pitcher with maximum capacity
+    capacities = np.append(capacities, sys.maxsize)
     
+    #initializing the capacities with zero values
+    initial_state = list(np.zeros(capacities.shape, dtype = np.int32))
+
+    #initial heuristic distance between the initial state and goal state
+    initial_heuristic = heuristic(initial_state,target_quantity)
+    
+    #initializing the step with zero
+    initial_step = 0
+
+    #initializing the fridge by adding the initial tuple
+    fridge_pq = [(initial_heuristic+initial_step, initial_step, initial_state)]
+
+    #initializing the visited_states set
     visited_states = set()
     
     while fridge_pq:
+
+        #popping the tuple from the priority queue
         _ , step, current_state =  heapq.heappop(fridge_pq)
 
+        #checking if the infinite capacity pitcher has a water or not
         if current_state[-1] == target_quantity:
            return step
         
+        #checking if infinite capacity pitcher has a higher value than target
         elif current_state[-1] > target_quantity:
             return -1
 
+        #adding the state to the visited_states set
         visited_states.add(tuple(current_state))
 
         for i in range(capacities.shape[0]):
             for j in range(capacities.shape[0]):
+                #if pitchers are same, we don't do anything just continue
                 if i==j:
                     continue
+
+                #copying the state for next state
                 next_state = current_state.copy()
+
+                #finding the minimum between capacities for pouring the water
                 pour = min(capacities[i],capacities[j])
+
+                #if we can pour from one cup to other without overflowing or underflowing
+                #and if it is not the inifinity capacity pitcher then we pour it
                 if next_state[i]+pour <= capacities[i] and next_state[j]-pour >= 0 and j!=capacities.shape[0]-1:
                     next_state[i]+=pour
                     next_state[j]-=pour
+                
+                #in other case, if it is not the infinity capacity pitcher, we just fill or unfill it with water.
                 else:
                     if i!=capacities.shape[0]-1:
                         next_state[i] = capacities[i] - next_state[i]
+
+                #if next state is not in visited_state then we can push the new state to the queue
                 if tuple(next_state) not in visited_states:
                     heapq.heappush(fridge_pq, (step+1+heuristic(next_state,target_quantity), 
                                    step+1, next_state))
 
     return -1
 
+
 def main():
-    filename = sys.argv[1]
+    #cheking if user added the name of file or not
     if len(sys.argv)==1:
         raise Exception("Please add the second argument for the name of the file")
+    
+    #reading the argument as filename
+    filename = sys.argv[1]
+
+    #checking the existence of the file
     if not os.path.isfile(filename):
         raise FileExistsError("File do not exist")
+    
+    #reading and getting the values of capacities and target quantity from file
     capacities, target_quantity = read_file(filename)
 
-    if np.size(capacities)==0:
-        raise Exception("There is no capacities in the first line of the file")
-    
-    if np.size(capacities)==0:
-        raise Exception("There is no target quantity in the second line of the file")
-    
+    #getting the result
     result = a_star_algorithm(capacities,target_quantity)
 
+    #printing the result
     if(result == -1):
         print("There is no path for the target quantity")
     else:
